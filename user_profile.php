93e4433402c0738ebb5adca30056c24f1f2771f4
Md Ashikul Islam <67536807+md-ashikul-islam@users.noreply.github.com>
2021-09-13 14:34:07 +0600

    Add files via upload

--- user_profile.php
+++ user_profile.php
@@ -66,14 +66,59 @@ if(isset($_GET['logoutuser'])){
             <h2>User Profile</h2>
             <div class="user_info">
                 <div class="user_details">
-                    <h3>Hello <?php echo $userName ?>!</h3>
-                    <a href="?logoutuser=logout">Logout</a>
+                    <h3>Hello <?php if(isset($userName)){echo strtoupper($userName);} ?>!</h3>                  
+                    <a href="?logoutuser=logout">Logout</a>             
                 </div>
+                <br>
                 <div class="history">
-                    
+                <div class="shopping-cart-container">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <h3 class="box-title">Order History</h3>
+                            <form class="shopping-cart-form" action="#" method="post">
+                                <table class="shop_table cart-form">
+                                    <thead>
+                                    <tr>
+                                        <th class="product-name">Product Name</th>
+                                        <th class="product-price">Total Paid</th>
+                                        <th class="product-subtotal">Order Status</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr class="cart_item">
+                                        <td class="product-thumbnail" data-title="Product Name">
+                                            <a class="prd-thumb" href="#">
+                                                <figure><img width="113" height="113" src="assets/images/shippingcart/pr-01.jpg" alt="shipping cart"></figure>
+                                            </a>
+                                            <a class="prd-name" href="#">National Fresh Fruit</a>
+                                        </td>
+                                        <td class="product-price" data-title="Price">
+                                            <div class="price price-contain">
+                                                <ins><span class="price-amount"><span class="currencySymbol">£</span>85.00</span></ins>
+                                            </div>
+                                        </td>
+                                        <td class="product-subtotal" data-title="Total">
+                                            Pending
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </form>
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
-        </div>     
+            <div class="container">
+        <div class="header-text-content text-center">
+            <div class="header-text wow bounceIn" data-wow-delay="0.6s">Welcome to <span>Biolife HTML</span></div>
+            <div class="header-text-2 wow fadeInUp" data-wow-delay="0.6s">The best templates for ecommerce websites!</div>
+            <a target="_blank" href="https://1.envato.market/qvr1O" class="btn-header purchase">Purchase</a>
+            <a href="#section-demo" class="btn-header">View Demos</a>
+        </div>
+    </div>
+        </div> 
+    </div>    
 
     <!-- FOOTER -->
     <?php include_once("includes/footer.php"); ?>
